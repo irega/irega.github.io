cdee4a3c6524b947ca6c00622614ee79e136c555
Iván Reinoso García <ireinoso@plainconcepts.com>
2021-05-03 23:00:10 +0200

    add avatar picture component

---
+++ packages/about-me/src/components/AvatarPicture/AvatarPicture.js
@@ -0,0 +1,8 @@
+import image from "../../assets/img/aboutme_avatar.jpg";
+import './AvatarPicture.css';
+
+const AvatarPicture = () => {
+  return <img className="image" src={image} alt="talking at JSDay Canary Islands 2019" />;
+};
+
+export { AvatarPicture };
