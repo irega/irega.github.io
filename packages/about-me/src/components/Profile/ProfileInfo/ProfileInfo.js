f3a8370ee69f7c69d3f3d42ffa7fc6fc303794ef
Ivan Reinoso <ivan.reinoso@taxfix.de>
2023-04-07 22:22:42 +0200

    update profile info

--- packages/about-me/src/components/Profile/ProfileInfo/ProfileInfo.js
+++ packages/about-me/src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -4,12 +4,12 @@ const ProfileInfo = () =>
   <div className="profile-info">
     <h1 className="profile-title">About me</h1>
     <p className="profile-details">
-      I'm a computer science and technology passionated since I was a child, and I feel lucky to work on it during 11 years.
-      After working with different languages and technologies related to the Microsoft ecosystem, in the last few years I have specialized
-      in Frontend technologies, becoming a reference in the last projects in which I have collaborated.
+      I'm a computer science and technology passionated since I was a child, and I feel lucky to work on it during 14 years.
+      After working with different languages and technologies related to Backend and Frontend along my career, I have become a 
+      reference in the last projects in which I have collaborated.
         </p>
     <p className="profile-details">
-      I am currently Frontend Technical Lead at Plain Concepts Madrid, always trying to learn and improve with my team mates,
+      I am currently Senior Software Engineer at Taxfix, always trying to learn and improve with my team mates,
       applying best practices, developing clean code and relying on the latest technologies.
         </p>
     <p className="profile-details">
