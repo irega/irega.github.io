263c3bfc866028c180c96533348fe8b86b7cdf28
Ivan Reinoso <ivan.reinoso@taxfix.de>
2023-04-19 21:28:17 +0200

    remove aboutme section

--- packages/about-me/src/components/Profile/ProfilePicture/ProfilePicture.js
+++
@@ -1,9 +0,0 @@
-import image from "../../../assets/img/aboutme_profile.jpg";
-import './ProfilePicture.css';
-
-const ProfilePicture = () =>
-  <span className="profile-img-section">
-    <img className="profile-img" src={image} alt="talking at JSDay Canary Islands 2019" />
-  </span>;
-
-export { ProfilePicture };
